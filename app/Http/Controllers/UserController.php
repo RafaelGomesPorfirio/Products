ec5becb69ec8594dc2f204633e0c36242b9b464f
Rafael <rafagomes475@gmail.com>
2023-01-16 16:16:30 -0300

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function login()
+    {
+        return view('login');
+    }
+
+    public function signin(Request $request)
+    {
+        $credentials = [
+            'email' => $request->email,
+            'password'=> $request->password
+        ];
+        
+        if (auth()->attempt($credentials)) {
+            return redirect('/');
+        }
+
+        return back();
+    }
+
+    public function register()
+    {
+        return view('register');
+    }
+
+    public function signup(Request $request)
+    {
+        User::create([
+            'name' => $request->name,
+            'email'=> $request->email,
+            'password' =>bcrypt($request->password)
+        ]);
+        
+        return redirect('/login');
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+        return redirect('/login');
+    }
+}
+
---
+++ routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Models\Product;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::group(['middleware' => 'auth'], function () {
+        Route::get('/', [ProductController::class, 'index'])->middleware('auth');
+
+        Route::get('/create', [ProductController::class, 'create']);
+
+        Route::post('/store',[ProductController::class, 'store']);
+
+        Route::get('/show/{id}', [ProductController::class, 'show'] );
+
+        Route::get('/edit/{id}', [ProductController::class, 'edit']);
+
+        Route::post('/update/{id}', [ProductController::class, 'update']);
+
+        Route::post('/destroy/{id}', [ProductController::class, 'destroy']);
+});
+// Rotas de autenticação 
+
+Route::get('/login', [UserController::class, 'login']) ->name('login');
+
+Route::post('/signin', [UserController::class, 'signin']);
+
+Route::get('/register', [UserController::class, 'register']);
+
+Route::post('/signup', [UserController::class, 'signup']);
+
+Route::get('/logout', [UserController::class, 'logout']);
